6a9f3414a22812046f710d2905df4829dfc69dad
Marcos Cruz <github@programandala.net>
2017-11-22 13:30:54 +0100

    Factor; use constants, variables, and full screen

--- serpentino.fs
+++ serpentino.fs
@@ -2,14 +2,14 @@
 
 \ Serpentino
 
-\ Description: A simple snake game project written in Forth for Gforth
+\ Description: A simple snake game written in Forth for Gforth
 \ and Solo Forth. Under development.
 
 \ Author: Marcos Cruz (programandala.net)
 \ http://programandala.net
 \ http://github.com/programandala-net/serpentino 
 
-\ Last modified 201711221228
+\ Last modified 201711221329
 
 \ =============================================================
 \ License
@@ -26,64 +26,84 @@
 
 \ ==============================================================
 
-: not ( x1 -- x2 ) true xor ;
+: random-range ( n1 n2 -- n3 ) over - utime + swap mod + ;
 
-: myrand ( n1 n2 -- n3 ) over - utime + swap mod + ;
+200 constant max-length
 
-: snake-size 200 ;
+cols 2 - constant arena-width
 
-: xdim 50 ;
+rows 4 - constant arena-height
 
-: ydim 20 ;
+2 cells constant /segment
 
-create snake snake-size cells 2 * allot
+create snake  max-length /segment * allot
+  \ Snake's segments. Each segment contains its coordinates.
 
-create apple 2 cells allot
+2variable apple
+  \ Coordinates of the apple.
 
 variable head
+  \ Head segment.
 
 variable length
+  \ Snake's current length.
 
 variable direction
+  \ Snake's current direction, as an execution token that
+  \ returns the coordinates' increments.
 
-: segment ( seg -- adr ) head @ + snake-size mod cells 2 * snake + ;
+: segment ( n -- a )
+  head @ + max-length mod /segment * snake + ;
 
-: pos+ ( x1 y1 x2 y2 -- x y ) rot + -rot + swap ;
+: pos+ ( x1 y1 x2 y2 -- x3 y3 ) rot + -rot + swap ;
 
-: point= 2@ rot 2@ rot = -rot = and ;
+: clash? ( a1 a2 -- f ) 2@ rot 2@ d= ;
+  \ Are the coordinates contained in _a1_ equal to the
+  \ coordinates contained in _a2_?
 
-: head* ( -- x y ) 0 segment ;
+: head* ( -- a ) 0 segment ;
+  \ _a_ is the address of the head segment.
 
-: move-head! ( -- ) head @ 1 - snake-size mod head ! ;
+: head? ( a -- f ) head* clash? ;
+  \ Does the head clash with segment _a_?
 
-: grow! ( -- ) 1 length +! ;
+: move-head ( -- ) head @ 1- max-length mod head ! ;
 
-: eat-apple ( -- )  1 xdim myrand 1 ydim myrand apple 2! grow! ;
+: grow ( -- ) 1 length +! ;
 
-: step! ( xdiff ydiff -- ) head* 2@ move-head! pos+ head* 2! ;
+: random-coordinates ( -- x y ) 1 arena-width  random-range
+                                1 arena-height random-range ;
 
-: left  ( -- n1 n2 ) -1  0 ;
+: new-apple ( -- ) random-coordinates apple 2! ;
 
-: right ( -- n1 n2 )  1  0 ;
+: eat-apple ( -- ) grow new-apple ;
 
-: down  ( -- n1 n2 )  0  1 ;
+: step! ( xdiff ydiff -- ) head* 2@ move-head pos+ head* 2! ;
 
-: up    ( -- n1 n2 )  0 -1 ;
+-1  0 2constant left
 
-: wall? ( -- f )
-  head* 2@ 1 ydim within swap 1 xdim within and not ;
+ 1  0 2constant right
+
+ 0  1 2constant down
+
+ 0 -1 2constant up
+
+: wall? ( -- f ) head* 2@ 1 arena-height within swap
+                          1 arena-width  within and 0= ;
 
 : crossing? ( -- f )
-  false length @ 1 ?do i segment head* point= or loop ;
+  length @ 1 ?do  i segment head? if unloop true exit then
+             loop false ;
 
-: apple? ( -- f ) head* apple point= ;
+: apple? ( -- f ) head* apple clash? ;
 
-: dead? wall? crossing? or ;
+: dead? ( -- f ) wall? crossing? or ;
 
 : draw-frame ( -- )
-  0 0 at-xy xdim 0 ?do ." +" loop
-            ydim 0 ?do xdim i at-xy ." +" cr ." +" loop
-            xdim 0 ?do ." +" loop cr ;
+  0 0 at-xy
+  arena-width  0 ?do ." +" loop
+  arena-height 0 ?do arena-width i at-xy ." +" cr ." +" loop
+  arena-width  0 ?do ." +" loop cr ;
 
 : draw-snake ( -- )
   length @ 0 ?do i segment 2@ at-xy ." #" loop ;
@@ -95,7 +115,7 @@ variable direction
 
 : init ( -- )
   0 head !
-  xdim 2 / ydim 2 / snake 2!
+  arena-width 2 / arena-height 2 / snake 2!
   3 3 apple 2!
   3 length !
   ['] up direction !
@@ -103,10 +123,10 @@ variable direction
 
 : (rudder) ( -- xt )
   key case
-    'a' of ['] left  endof
-    'w' of ['] up    endof
-    'd' of ['] right endof
-    's' of ['] down  endof
+    '4' of ['] left  endof
+    '6' of ['] up    endof
+    '5' of ['] right endof
+    '7' of ['] down  endof
     direction @ swap
   endcase ;
 
@@ -124,12 +144,13 @@ init
 
 page
 ." Serpentino"
-3000 ms
+2000 ms
 200 game
 
 \ =============================================================
 \ Change log
 
 \ 2017-11-22: Fork from Robert Pfeiffer's forthsnake
-\ (https://github.com/robertpfeiffer/forthsnake). Change source style.
-\ Rename words. Factor the main loop.
+\ (https://github.com/robertpfeiffer/forthsnake). Change source
+\ style.  Rename words. Factor. Use constants and variables.
+\ Use full screen.
